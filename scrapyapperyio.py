5034377969fbc9e49e73f23f782a93bcfdfa7efe
Dimitrios Kouzis-Loukas <lookfwd@gmail.com>
2014-10-08 01:46:47 +0100

    converted to asynchronous

--- scrapyapperyio.py
+++ scrapyapperyio.py
@@ -1,4 +1,4 @@
-# Copyright 2013 Dimitrios Kouzis-Loukas <info@scalingexcellence.co.uk>.
+# Copyright 2013-14 Dimitrios Kouzis-Loukas <info@scalingexcellence.co.uk>.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,25 +14,60 @@
 
 """Appery.io Database Pipeline for scrapy"""
 
+from twisted.internet import defer
+from scrapy.http import Request
+from urllib import urlencode
+from scrapy import signals
 import json
-import requests
-from scrapy.conf import settings
+
 
 class ApperyIoPipeline(object):
 
-    def __init__(self):
-	url = "https://api.appery.io/rest/1/db/login"
-	headers = { "X-Appery-Database-Id": settings['APPERYIO_DB_ID'] }
-	params = { "username": settings['APPERYIO_USERNAME'], "password": settings['APPERYIO_PASSWORD'] }
-	v = requests.get(url, headers=headers, params=params)
-        self.sessionToken = v.json()['sessionToken']
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
 
+    def __init__(self, crawler):
+        self.crawler = crawler
+        self.settings = crawler.settings
+        self.session = None
+
+        crawler.signals.connect(
+            self.spider_opened,
+            signal=signals.spider_opened
+        )
+
+    def spider_opened(self, spider):
+        request = Request(
+            "https://api.appery.io/rest/1/db/login?{0}".format(urlencode({
+                "username": self.settings.get('APPERYIO_USERNAME'),
+                "password": self.settings.get('APPERYIO_PASSWORD')
+            })),
+            headers={
+                "X-Appery-Database-Id": self.settings.get('APPERYIO_DB_ID')
+            }
+        )
+        self.session = self.crawler.engine.download(request, spider)
+        self.session.addCallback(lambda r: json.loads(r.body)['sessionToken'])
+
+    @defer.inlineCallbacks
     def process_item(self, item, spider):
-        url = "https://api.appery.io/rest/1/db/collections/%s" % settings['APPERYIO_COLLECTION_NAME']
-	headers = {
-		'Content-type': 'application/json',
-		'X-Appery-Database-Id': settings['APPERYIO_DB_ID'],
-		'X-Appery-Session-Token': self.sessionToken
-	}
-	requests.post(url, data=json.dumps(dict(item)), headers=headers)
-	return item
+        session = yield self.session
+
+        flat_item = dict([(k, v and v[0] or None) for k, v in item.items()])
+
+        request = Request(
+            "https://api.appery.io/rest/1/db/collections/{0}".format(
+                self.settings.get('APPERYIO_COLLECTION_NAME')
+            ),
+            method='POST',
+            headers={
+                'Content-type': 'application/json',
+                'X-Appery-Database-Id': self.settings.get('APPERYIO_DB_ID'),
+                'X-Appery-Session-Token': session
+            },
+            body=json.dumps(flat_item, default=lambda x: None)
+        )
+        yield self.crawler.engine.download(request, spider)
+
+        defer.returnValue(item)
